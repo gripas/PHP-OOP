70f3d62843e17c9814b1058b6c2b374c9bb23963
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-02-01 17:03:59 +0200

    Update config.php

--- User-Login-Register-OOP-PHP/config.php
+++ User-Login-Register-OOP-PHP/config.php
@@ -3,7 +3,5 @@ $host = "localhost";
 $username = "root";
 $password = ""; // Jūsų duomenų bazės slaptažodis / Your Database password
 $database = "educ";
-
 $connectDB= new mysqli($host,$username,$password,$database)or die("Nėra tinkamos jungties su Mysql".mysqli_error($connectDB));
-
 ?>
